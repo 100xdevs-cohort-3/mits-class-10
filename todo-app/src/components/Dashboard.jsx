57ee5d91220c3ff358af585c9c4fa78d3e4c8ba0
harkirat <harkirat96@gmail.com>
2025-07-03 12:58:16 +0530

    Structured fe

--- todo-app/src/components/Dashboard.jsx
+++ todo-app/src/components/Dashboard.jsx
@@ -1,73 +1,15 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-
+import { useTodos } from "../hooks/useTodos";
+import { CreateTodo } from "./CreateTodo";
+import { Todos } from "./Todos";
 
 function Dashboard() {
-    const [todos, setTodos] = useState([]);
-
-    useEffect(function() {
-        axios.get("http://localhost:3000/todos", {
-            headers: {
-                token: localStorage.getItem("token")
-            }
-        }).then(function(res) {
-            setTodos(res.data.todos);
-        })
+    const [todos, setTodos] = useTodos();
     
-    }, [])
-
-    function addTodo() {
-        let todo = document.getElementById("todo").value;
-        setTodos([...todos, todo]);
-        axios.post("http://localhost:3000/todos", {
-            todo
-        }, {
-            headers: {
-                token: localStorage.getItem("token")
-            }
-        })
-    }
-
-    return (
-        <div>
-            <div style={{
-                display: "flex", justifyContent: "center"
-            }}>
-                <input style={{
-                    margin: 20,
-                    padding: 10
-                }} id="todo" type="text" placeholder="todo" />
-                <div style={{display: "flex", flexDirection: "column", justifyContent: "center"}}>
-                    <button onClick={addTodo}>Submit todo</button>
-                </div>
-            </div>
-
-            <div style={{
-                display: "flex",
-                justifyContent: "center"
-            }}>
-                <input style={{
-                    margin: 20,
-                    padding: 10
-                }} placeholder="Search" />
-            </div>
-
-
-            <div style={{
-                display: "flex",
-                justifyContent: "center"
-            }}>
-                <div>
-                    {todos.map(todo => <div> <div>
-                        {todo}
-                    </div>
-                    <br/>
-                    </div>)}
-                </div>
-            </div>
-
-        </div>
-    )
+    return <div>
+        <CreateTodo todos={todos} setTodos={setTodos} />
+        <Todos todos={todos} />
+    </div>
 }
+
 
 export default Dashboard;
